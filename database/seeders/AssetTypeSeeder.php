d0f5bfa290e96452aa53ac24ad4e2b2e37d405b2
RiskiJayaPutra <rizkijayaputra2005@gmail.com>
2026-07-31 14:21:43 +0700

    fix: restore modern nested group schema format for AssetTypeSeeder to fix missing header groups in UI

--- database/seeders/AssetTypeSeeder.php
+++ database/seeders/AssetTypeSeeder.php
@@ -12,15 +12,22 @@ class AssetTypeSeeder extends Seeder
         $types = [
             [
                 'name' => 'Aset IT',
-                'slug' => 'it',
+                'slug' => 'aset-it',
                 'icon' => 'fa-computer',
                 'schema' => [
                     'columns' => [
                         ['key' => 'asset_number', 'label' => 'Nomor Asset', 'type' => 'text', 'mono' => true],
                         ['key' => 'serial_number', 'label' => 'Serial Number', 'type' => 'text', 'mono' => true],
                         ['key' => 'device_type', 'label' => 'Jenis Perangkat', 'type' => 'select', 'bold' => true, 'options' => ['PC', 'PC AIO', 'Laptop', 'Monitor', 'Printer', 'Scanner']],
-                        ['key' => 'user_number', 'label' => 'Nomor Pengguna', 'type' => 'text'],
-                        ['key' => 'user_name', 'label' => 'Nama Pengguna', 'type' => 'text'],
+                        [
+                            'key' => 'pengguna',
+                            'type' => 'group',
+                            'label' => 'Pengguna',
+                            'subColumns' => [
+                                ['key' => 'nama', 'type' => 'text', 'label' => 'Nama'],
+                                ['key' => 'nipp', 'type' => 'number', 'label' => 'NIPP'],
+                            ]
+                        ]
                     ]
                 ]
             ],
@@ -47,24 +54,29 @@ class AssetTypeSeeder extends Seeder
                     'columns' => [
                         ['key' => 'train_number', 'label' => 'Nomor Kereta', 'type' => 'text', 'mono' => true],
                         ['key' => 'train_type', 'label' => 'Type Kereta', 'type' => 'text'],
-                        
-                        // Status CCTV sub-columns
-                        ['key' => 'cctv_ip', 'label' => 'IP', 'subLabel' => 'IP', 'type' => 'radio', 'radioValue' => 'ip', 'radioGroup' => 'cctv_type', 'radioGroupKey' => 'cctv_type', 'headerGroup' => 'status_cctv', '_skipHeader' => true],
-                        ['key' => 'cctv_analog', 'label' => 'Analog', 'subLabel' => 'Analog', 'type' => 'radio', 'radioValue' => 'analog', 'radioGroup' => 'cctv_type', 'radioGroupKey' => 'cctv_type', 'headerGroup' => 'status_cctv', '_skipHeader' => true],
-                        
-                        // Recorder sub-columns
-                        ['key' => 'recorder_dvr', 'label' => 'DVR', 'subLabel' => 'DVR', 'type' => 'radio', 'radioValue' => 'dvr', 'radioGroup' => 'recorder_type', 'radioGroupKey' => 'recorder_type', 'headerGroup' => 'recorder', '_skipHeader' => true],
-                        ['key' => 'recorder_nvr', 'label' => 'NVR', 'subLabel' => 'NVR', 'type' => 'radio', 'radioValue' => 'nvr', 'radioGroup' => 'recorder_type', 'radioGroupKey' => 'recorder_type', 'headerGroup' => 'recorder', '_skipHeader' => true],
-                        ['key' => 'recorder_standalone', 'label' => 'Standalone', 'subLabel' => 'Standalone', 'type' => 'radio', 'radioValue' => 'standalone', 'radioGroup' => 'recorder_type', 'radioGroupKey' => 'recorder_type', 'headerGroup' => 'recorder', '_skipHeader' => true],
-                        
+                        [
+                            'key' => 'group_status_cctv',
+                            'type' => 'group',
+                            'label' => 'Status CCTV',
+                            'subColumns' => [
+                                ['key' => 'cctv_ip', 'type' => 'radio', 'label' => 'IP', 'subLabel' => 'IP', 'radioGroup' => 'cctv_type', 'radioGroupKey' => 'cctv_type', 'radioValue' => 'ip', '_skipHeader' => true],
+                                ['key' => 'cctv_analog', 'type' => 'radio', 'label' => 'Analog', 'subLabel' => 'Analog', 'radioGroup' => 'cctv_type', 'radioGroupKey' => 'cctv_type', 'radioValue' => 'analog', '_skipHeader' => true],
+                            ]
+                        ],
+                        [
+                            'key' => 'group_recorder',
+                            'type' => 'group',
+                            'label' => 'Recorder',
+                            'subColumns' => [
+                                ['key' => 'recorder_dvr', 'type' => 'radio', 'label' => 'DVR', 'subLabel' => 'DVR', 'radioGroup' => 'recorder_type', 'radioGroupKey' => 'recorder_type', 'radioValue' => 'dvr', '_skipHeader' => true],
+                                ['key' => 'recorder_nvr', 'type' => 'radio', 'label' => 'NVR', 'subLabel' => 'NVR', 'radioGroup' => 'recorder_type', 'radioGroupKey' => 'recorder_type', 'radioValue' => 'nvr', '_skipHeader' => true],
+                                ['key' => 'recorder_standalone', 'type' => 'radio', 'label' => 'Standalone', 'subLabel' => 'Standalone', 'radioGroup' => 'recorder_type', 'radioGroupKey' => 'recorder_type', 'radioValue' => 'standalone', '_skipHeader' => true],
+                            ]
+                        ],
                         ['key' => 'monitor', 'label' => 'Monitor', 'type' => 'text'],
                         ['key' => 'quantity', 'label' => 'Jumlah', 'type' => 'number'],
                         ['key' => 'condition', 'label' => 'Kondisi', 'type' => 'status', 'options' => ['Baik', 'Perawatan', 'Rusak']],
                         ['key' => 'description', 'label' => 'Keterangan', 'type' => 'text'],
-                    ],
-                    'headerGroups' => [
-                        ['id' => 'status_cctv', 'label' => 'Status CCTV', 'colSpan' => 2],
-                        ['id' => 'recorder', 'label' => 'Recorder', 'colSpan' => 3],
                     ]
                 ]
             ],
